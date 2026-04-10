6c8f75d15d50a395df2b9c375f14314708f10c8d
tulu-g559 <garnab559@gmail.com>
2026-04-10 14:57:22 +0530

    test pr2

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="wrathops",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=["wrathops_cli", "wrathops_cli.*", "shared", "shared.*"]),
+    include_package_data=True,
     install_requires=[],
     entry_points={
         "console_scripts": [
             "wrathops=wrathops_cli.main:main"
         ]
-    }
+    },
 )
