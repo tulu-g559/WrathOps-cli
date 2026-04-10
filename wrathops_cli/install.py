4ac4d17ea62353c9f79f72a588c1ee753e101138
tulu-g559 <garnab559@gmail.com>
2026-04-10 15:32:31 +0530

    test pr7

--- wrathops_cli/install.py
+++ wrathops_cli/install.py
@@ -3,7 +3,7 @@ import os
 HOOK_SCRIPT = """#!/bin/sh
 echo "🔍 WrathOps scanning..."
 
-python -m wrathops_cli.main scan
+"C:/Users/ayonp/AppData/Local/Packages/PythonSoftwareFoundation.Python.3.13_qbz5n2kfra8p0/LocalCache/local-packages/Python313/Scripts/wrathops.exe" scan
 
 STATUS=$?
 
